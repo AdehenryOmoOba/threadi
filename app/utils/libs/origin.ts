688da7f0ff18bb7e7c06ff7b9eb678e8b9cbb9f3
adehenry <adefemiadelaja@gmail.com>
2023-08-08 22:17:33 -0700

    initial setup

--- app/utils/libs/origin.ts
+++ app/utils/libs/origin.ts
@@ -1,6 +1,13 @@
+// let origin =
+//   process.env.NODE_ENV !== "production"
+//     ? "http://localhost:3000/"
+//     : "https://shopi-ten.vercel.app/";
+
+// export default origin;
+
 let origin =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/"
-    : "https://shopi-ten.vercel.app/";
+    : "http://localhost:3000/";
 
 export default origin;
