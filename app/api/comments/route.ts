8e6317184f94bf970014268dbc5e8112fa33763a
adehenry <adefemiadelaja@gmail.com>
2023-08-16 13:50:45 +0100

    post threadi login completed

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -9,36 +9,36 @@ export async function GET(req: NextRequest){
 
     const id = new URL(req.url).searchParams.get("id")
 
-    try {
-        if(!id) throw new Error("Please provide id")
+    // try {
+    //     if(!id) throw new Error("Please provide id")
 
-        const preparedComments = db.query.threadis.findMany({
-          where: (eq(threadis.parentId, placeholder("id"))),
-          orderBy: desc(threadis.createdAt),
-          with: {
-              authorInfo: {
-                  columns: {
-                      uuid: true,
-                      name: true,
-                      image: true
-                  }
-              },
-              communitiesInfo: {
-                  columns: {
-                      uuid: true,
-                      name: true,
-                      image: true
-                  }
-              }
-          },
-        }).prepare("prepared_comments")
+    //     const preparedComments = db.query.threadis.findMany({
+    //       where: (eq(threadis.parentId, placeholder("id"))),
+    //       orderBy: desc(threadis.createdAt),
+    //       with: {
+    //           authorInfo: {
+    //               columns: {
+    //                   uuid: true,
+    //                   name: true,
+    //                   image: true
+    //               }
+    //           },
+    //           communitiesInfo: {
+    //               columns: {
+    //                   uuid: true,
+    //                   name: true,
+    //                   image: true
+    //               }
+    //           }
+    //       },
+    //     }).prepare("prepared_comments")
   
-        const comments = await preparedComments.execute({id})
+    //     const comments = await preparedComments.execute({id})
   
-        if(!comments) throw new Error("no comments found")
+    //     if(!comments) throw new Error("no comments found")
   
-        return NextResponse.json(comments)
-    } catch (error: any) {
-        return NextResponse.json({error: error.message})
-    }
+    //     return NextResponse.json(comments)
+    // } catch (error: any) {
+    //     return NextResponse.json({error: error.message})
+    // }
 }
