aa9d320655459347a4bce75910eddee38feec77e
adehenry <adefemiadelaja@gmail.com>
2023-09-05 17:06:28 +0100

    added loading button animations

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -15,8 +15,8 @@ function UserCard({id, name, imgUrl, userType, email}: Props) {
   const userhandle = `@${email}`.split("@")[0]
   return (
     <article className='user-card'>
-      <div className='user-card_avatar h-12 w-12 rounded-full overflow-hidden'>
-        <Image src={imgUrl} alt={name} width={48} height={48} className='rounded-full' />
+      <div className='user-card_avatar h-12 w-12 object-cover'>
+        <Image src={imgUrl} alt={name} fill className='rounded-full object-cover' />
         <div className="flex-1 text-ellipsis">
           <h4 className='text-base-semibold text-light-1'>{name}</h4>
           <p className='text-small-medium text-gray-1'>{userhandle}</p>
